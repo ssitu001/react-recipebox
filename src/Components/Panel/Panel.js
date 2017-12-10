be6089cd1d25da546cdc886c546734598930ed88
Simon Situ <ssitu001@gmail.com>
2017-12-10 10:45:08 -0800

    panel header more dynamic

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
 
+import image from '../../recipe-logo.png';
+
 import ListGroupComponent from '../ListGroup/ListGroup';
 
 const PanelComponent = (props) => {
   
   const {position, name, ingredients, handleDelete, openModal, closeModal} = props;
 
+  const panelHeader = (
+    <div>
+      <h3>{name}</h3>
+    </div>
+  )
+
   return (
     <Panel 
       bsStyle="danger" 
       collapsible 
-      header={name}>
+      header={panelHeader}
+      >
       <ListGroupComponent 
         position={position} 
         ingredients={ingredients}
