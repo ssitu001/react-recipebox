baf461ff0a8c20268b17d0bf8c7d97a856010ccd
Simon Situ <ssitu001@gmail.com>
2017-12-09 17:45:34 -0800

    list group working

--- src/Components/ListGroup/ListGroup.js
+++ src/Components/ListGroup/ListGroup.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
+import './ListGroup.css';
+
 const ListGroupComponent = (props) => {
-  // console.log('props', props)
-  const { position, ingredients, handleDelete, openModal, closeModal} = props;
+  const { 
+    position,
+    ingredients, 
+    handleDelete, 
+    openModal } = props;
+
   const listItem = ingredients.map((ingredient, i) => {
     return <ListGroupItem key={i}>{ingredient}</ListGroupItem>
   });
+  
   return (
     <ListGroup>
       {listItem}
       <div className="panel-button-group">
         <Button
+          className="recipe-btn"
           onClick={() => {handleDelete(position)}} 
-          bsStyle="danger">
+          bsStyle="info">
           Delete
         </Button>
         <Button 
+          className="recipe-btn"
           onClick={() => openModal('Edit Recipe', position)} 
           bsStyle="warning">
           Edit
