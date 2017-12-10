54e6246157a9d892c51136ab2caa6af8908952bb
Simon Situ <ssitu001@gmail.com>
2017-12-09 17:44:32 -0800

    formgroup component working

--- src/Components/FormGroup/FormGroup.js
+++ src/Components/FormGroup/FormGroup.js
@@ -1,60 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-class FormComponent extends Component {
-  state = {
-    name: '',
-  }
+import './FormGroup.css';
 
-  render() {
-    console.log('formprops', this.props)
+const FormComponent = (props) => {
+    console.log('formprops', props)
     const {
       currentRecipeName, 
       currentRecipeIngredients, 
       handleRecipe,
       modalType,
-    } = this.props;
+    } = props;
 
     return (
       <form>
         {
           modalType === 'Add Recipe'
           ? <FormGroup>
-          <ControlLabel>Recipes</ControlLabel>
-          <FormControl
-            type="text"
-            placeholder="Recipe Name"
-            onChange={handleRecipe}
-          />
-          <ControlLabel>Ingredients</ControlLabel>
-          <FormControl
-            componentClass="textarea"
-            placeholder="Ingredients"
-            onChange={handleRecipe}
-          />
-        </FormGroup>
-        : <FormGroup>
-            <ControlLabel>Recipes</ControlLabel>
-            <FormControl
-              type="text"
-              value={currentRecipeName}
-              name="currentRecipeName"
-              // placeholder="Recipe Name"
-              onChange={handleRecipe}
-            />
-            <ControlLabel>Ingredients</ControlLabel>
-            <FormControl
-              componentClass="textarea"
-              value={currentRecipeIngredients}
-              // placeholder="Ingredients"
-              name="currentRecipeIngredients"
-              onChange={handleRecipe}
-            />
-          </FormGroup>
+              <ControlLabel>Recipes</ControlLabel>
+                <FormControl
+                  type="text"
+                  placeholder={currentRecipeName}
+                  name="currentRecipeName"
+                  onChange={handleRecipe}
+                />
+              <ControlLabel>Ingredients</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  placeholder={currentRecipeIngredients}
+                  name="currentRecipeIngredients"
+                  onChange={handleRecipe}
+                />
+            </FormGroup>
+          : <FormGroup>
+              <ControlLabel>Recipes</ControlLabel>
+              <FormControl
+                type="text"
+                value={currentRecipeName}
+                name="currentRecipeName"
+                onChange={handleRecipe}
+              />
+              <ControlLabel>Ingredients</ControlLabel>
+              <FormControl
+                componentClass="textarea"
+                value={currentRecipeIngredients}
+                name="currentRecipeIngredients"
+                onChange={handleRecipe}
+              />
+            </FormGroup>
         }
       </form>
     )
-  }
 }
 
 export default FormComponent;
